b065447e5eb5bdbcce58c074adb3d4dcf71dd97e
xlzd <i@xlzd.me>
2015-11-18 23:44:22 +0800

    init

---
+++ xtls/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+"""
+init
+"""
+
+__author__ = 'xlzd'
+__version__ = '0.0.1'
+
+__all__ = []
+
+
+def main():
+    print 'hello, world'
+
+
+if __name__ == '__main__':
+    main()
+
