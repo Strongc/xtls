54a87fa7758166db7a775a3d0fb803272cc9ba03
xu.du <xu.du@socialcredits.cn>
2015-11-19 14:38:28 +0800

    errors

---
+++ xtls/errors.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+
+class ConsumerFatalError(Exception):
+    """
+    当分布式爬虫遇到无法后续任务的异常时，抛出这个异常以停止分配后续任务
+    """
+    pass
+
+
+class ProducerFatalError(Exception):
+    """
+    当分布式爬虫的任务分配遇到无法后续分配的异常时，抛出这个异常以停止后续的操作
+    """
+    pass
