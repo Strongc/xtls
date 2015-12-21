e877a81987eff3fa3207ba8cf67b308eb04ca05f
xlzd <i@xlzd.me>
2015-12-21 18:38:40 +0800

    汉字转拼音

--- xtls/__about__.py
+++ xtls/__about__.py
@@ -28,7 +28,7 @@ __requires__ = [
     # 'cassandra-driver',
 ]
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 __name__ = 'xtls'
 __summary__ = 'xtls: tools by xlzd'
---
+++ xtls/pinyin.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+"""
+descprition
+"""
+
+__author__ = 'xlzd'
+
+
+WORD_DICT = {}
+
+with open('letters.data', 'r') as f:
+    for f_line in f:
+        try:
+            line = f_line.split('    ')
+            WORD_DICT[line[0]] = line[1]
+        except:
+            line = f_line.split('   ')
+            WORD_DICT[line[0]] = line[1]
+
+
+def _parse_letter(char, only_first):
+    letter = WORD_DICT.get('%X' % ord(char), char).split()[0]
+    return (letter[:-1], letter[0])[only_first], letter[-1]
+
+
+def parse(string, only_first=False):
+    if not isinstance(string, unicode):
+        string = string.decode('utf-8')
+
+    return [_parse_letter(char, only_first) for char in string]
